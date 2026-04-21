6d6902926dc2810ca32ce1e823e7280939c1e8bf
imasakimimaru-commits <imasakimimaru@gmail.com>
2026-04-21 10:43:20 +0900

    今崎心丸

---
+++ client/page/touroku.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登録画面</title>
+</head>
+<body>
+    <h1>登録画面</h1>
+
+    <form action="safetyList.php" method="post">
+        <input type="checkbox" name="safety" value="安全">安全
+        <input type="checkbox" name="safety" value="危険">危険<br>
+        <label for="date">日付を選択してください:</label>
+        <input type="date" id="date" name="date">
+        <input type="submit" value="送信">
+    </form>
+
+
+
+    <!-- <a href="safetyList.php">社員安否一覧画面</a> -->
+</body>
+</html>
